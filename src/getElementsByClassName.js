c8e847bc20e9871fa862aa2f85e09ffa27242d8a
Autumn Fjeld <autumn@nmutua.com>
2013-12-09 17:54:30 -0800

    push any matched elements into array.  This version not passing the specs, but functionality seems closer.  Get stack overflow, which googling shows this will need to be handled differently than this version

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,47 +5,44 @@
 
 // But in stead we're going to implement it from scratch:
 
-// Assuming we can use underbar
-/*
-var getElementsByClassName = function (className) {
-	console.log('**** Let\'s START:', 'typeof:', typeof(className), 'className:', className);
-	var huh = document.getElementsByClassName(className);
-	console.log('huh', huh);
-	console.log('direct', document.getElementsByClassName('targetClassName') );
-	return document.getElementsByClassName(className);
-
-};
-*/
+// Outline:
+// produce an array that contains all elements which contain the specified class
+// Loop thru document object recursively to find all instances of className
+// document object contains strings and objects
+// CANNOT do a straight recursion loop into object, this results in stack overflow
+// hasOwnProperty
 
 var getElementsByClassName = function (className) {
   console.log('**** Let\'s START:', className);
 	//  console.log(typeof(document));
-	var obj = window.document;
-	console.log(obj);
+	var obj = window.document;   // this is both a node and an object
+	var value = null;
+	var result = [];
+
 
   var getClassName = function(obj) {
   	console.log('CHECK', obj.className);
 
   	if (obj.className === className) {
   		console.log("Found it :", obj.className);  
-  		return "HI";	
+  		result.push(obj.className);  
   		console.log("this should never show")	;
   	}
   	else {
-    	_.each(obj, function(value, key) {
-  		  if (typeof(value) === 'object') {
-  		  	console.log('Recurse!! value', value);
-  		  	getClassName(value);
-  		  }
-    	}); 
+   	 	_.each(obj, function(value, key) {
+        if (typeof(value) === 'object' && value !== null) {
+          console.log('Recurse!! value', value);
+          getClassName(value);
+        }
+      }); 
     }
   };
   
   var result = getClassName(obj);   // i think this immediately calls the function ???
 
 	//console.log('EXPECTED', document.getElementsByClassName('targetClassName') );
-	console.log('EXPECTED2', window.document.getElementsByClassName('targetClassName') );
-	//console.log('result: ', result);
+	//console.log('EXPECTED2', window.document.getElementsByClassName('targetClassName') );
+	console.log('result: ', result);
 	//return getClassName(obj);  
 	return result;
 };
