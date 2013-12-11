7ab5be12ef11a956af70d22e1d76b6e27fc9669b
Autumn Fjeld <autumn@nmutua.com>
2013-12-11 11:26:11 -0800

    Removed white() function for simplicity.  Not passing first spec, function not returning parse value, need to find out why

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -5,26 +5,25 @@
 // Use a recursive descent parser
 
 var parseJSON = function (json) {
-	console.log('*** START json is:', json);
-	var	text = json;
-	var at = 0;							// index of current character
-	var ch;						// current character
+
+	console.log('%c*** START json is:', 'background-color:yellow;', json, 'typeof is', typeof json);
+	
+	var	text;
+	var at;							// index of current character
+	var ch;							// current character
 
 	// Move to next character
 	next = function () {
-		ch = text.charAt(at);
-		console.log('at:',at, 'ch:', ch);
 		at++
+		ch = text.charAt(at);
+		console.log("IN next. ch:", ch, 'at', at, 'text', text);
 		return ch;
 	}
 
-	white = function () {
-		console.log("IN white");
-		next();
-	};
 
 	// Parse a number
 	var number = function () {
+		console.log("IN number. ch is", ch );
 		var number;
 		var string = '';
 
@@ -34,11 +33,13 @@ var parseJSON = function (json) {
 		}
 
 		number = +string;  			// why not just return string? and what is + here?
+		console.log("is this a number", typeof number);
 		return number;
 	};
 
+	// Parse a string (does not account for escaped characters)
 	var string = function () {
-		//console.log("IN word. ch is", ch, 'test', ch < 'z');
+		console.log("IN string. ch is", ch, 'test', ch < 'z');
 		var string = '';
 
 		while ( ch >= 'A' && ch <= 'z') {
@@ -46,29 +47,42 @@ var parseJSON = function (json) {
 			next();
 		}
 
-		console.log("end word is:", string);
 		return string;
+	};
+
+	// Parse special words
+	var specialWords = function () {
+
 	};
 
 	var value = function () {
 		console.log('IN value. ch:', ch);
-		white();
 		switch (ch) {
 			case null:
-				return null;
+				return 'HI';
 			default: 
 				return ch >= '0' && ch <= '9' ? number() : string();
 		}
 
 	};
 
-	return function () {
-		ch = ' ';
+	var result = function () {  
+		// Initialize stuff here
+		text = json;
+		at = 0;
+		ch = text[0];
 		//console.log("here");
-		result = value();
-		console.log('JSON.parse is', JSON.parse(json));
-		console.log('my result', result);
-		return result;
-	}();
+		var parsed = value();
+		console.log('%cJSON.parse is: ', 'background-color: pink;', JSON.parse(json), typeof JSON.parse(json));
+		console.log('%cmy parse is:', 'background-color: pink;', parsed, ' and typeof', typeof parsed);
+
+		return parsed;
+	};
+
+	//return 'autumn';
+	return result();  //why is the return value of parsed not returned for parseJSON ?
+	
+
+
 
 };
