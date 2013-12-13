391c1ca896d46c5376b1609788ac9efa1bfe5bac
Autumn Fjeld <autumn@nmutua.com>
2013-12-13 12:30:55 -0800

    Cleaned up comments and console.logs

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,35 +2,29 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to have to write it from scratch:
+// Function, RegExp, and Error objects and undefined value cannot be serialized
+
 var stringifyJSON = function (obj) {
 	args = Array.prototype.slice.call(arguments);
-	//console.log('args', args);
 	var valid = null;
 	var strObj = null;
 
 	var getElement = function(obj) {
 		if (Array.isArray(obj)) {
-			//console.log('in array');
 			obj = _.map(obj, getElement);
-			//console.log('stuff', obj);
 			return "[" + obj + "]";
 		}
 		else if (typeof(obj) === 'object' && obj !== null) {
 				strObj = "";
 			_.each(obj, function(value, key) {
-				//console.log('key',key, 'value', value);
-				// Function, RegExp, and Error objects and undefined value cannot be serialized
 				valid = (typeof(value) === 'function' || value === void 0) ? false : true;
-				//console.log('valid',valid);
 				if (!valid) {
 					strObj = '';
 				} 
 				else {
 				strObj = strObj + ',' + getElement(key) + ":" + getElement(value);					
 				}
-				//console.log('changing object ', strObj);
 			});
-			//console.log('end of object processing.  obj is', strObj)
 			return   "{" + strObj.substring(1) + "}";
 		}
 		else if (typeof(obj) === 'function' || obj === void 0) {
@@ -41,8 +35,6 @@ var stringifyJSON = function (obj) {
 		}
 		else if (typeof(obj) === 'number') {
 			return obj.toString();
-			//console.log('in number', item);
-			//return item;
 		} 
 		else if (typeof(obj) === 'string') {
 			return '"' + obj + '"' ;
@@ -51,14 +43,7 @@ var stringifyJSON = function (obj) {
 		else if (typeof(obj) === 'boolean') {
 			return obj.toString();
 		}
-		//else
-			//return "No match found. FIGURE IT OUT";
 	};
 	
 	return getElement(obj);
-
-	//console.log('item: ', item, 'count',count)
-	//return item;
-
-  // your code goes here
 };
