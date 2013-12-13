e7ba11a5292404172fa784f7bf867e98c97f3bcf
Autumn Fjeld <autumn@nmutua.com>
2013-12-13 12:27:32 -0800

    Cleaned up console.log and comments

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -14,47 +14,36 @@
 
 
 var getElementsByClassName = function (className) {
-
-  //console.log('**** Let\'s START:', className);
-	var obj = window.document.body;   						// this is an element, a node, and an object
+	var obj = window.document.body;   					
 	var result = [];
 
 	var lookForClass = new RegExp("\\b" + className + "\\b");
 
   var getClassName = function(obj) {
-  	//console.log('CHECK', obj.classList, 'and obj is:', obj);
 
   	if (lookForClass.test(obj.classList)) {
-  		//console.log("Found it :", obj.classList);  
   		result.push(obj);  
   	}
   	
 		var children = obj.childNodes;					// returns collection of node-objects, NOT an array 
-		//console.log('child', obj.childNodes, 'children array?', Array.isArray(children));
 
    	_.each(children, function(value, key) {
-   		//console.log('value is', value);
       if (value.nodeType != 1) {					// if not an element node skip this iteration
-      	return;														// why illegal continue statement? continue or return?!?
+      	return;												
       }
-      //console.log('Recurse!! value', value);
       getClassName(value);
     }); 
- 	
- 	//return true;  												// is this necessary?
+
   };
   
  	getClassName(obj); 
 
-	//console.log('EXPECTED', window.document.getElementsByClassName('targetClassName') );
-	//console.log('result: ', result);		// Why isn't this displayed in console?
- 
 	return result;
 };
 
 
 /*
-// Using walk the DOM approach.
+// ToDo:  Using walk the DOM approach.
 var getElementsByClassName = function (className) {
 
 };
